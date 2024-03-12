cb611a110bd7ccd78e7eff988a37c540519f8719
Kristen <kristenarguello20@gmail.com>
2024-03-12 00:07:53 -0300

    Add conversion and some easy flags to logic

--- headers/ieee-stuff.h
+++ headers/ieee-stuff.h
@@ -1,7 +1,7 @@
 char* floatToBinary(float f);
-
-int isInexact(float resultado); //recebe o resultado
-int isDivByZero(float segundoNumero); //recebe o segundo numero
-int isUnderflow(float resultado); //recebe o resultado
-int isOverflow(float resultado); //recebe o resultado
+int isInexact(float resultado, int* firstRounded, int* secondRounded); //recebe o resultado
+void isDivByZero(float primeiroNumero, float segundoNumero, int* divZero); //recebe o segundo numero
+int infinitySign(float primeiroNumero);
+void isUnderflow(float result, float a, float b, int* underflow); //recebe o resultado
+void isOverflow(float result, float a, float b, int* overflow); //recebe o resultado
 int isInvalidOperation(float resultado); //recebe o resultado?
--- ieee-stuff.c
+++ ieee-stuff.c
@@ -1,29 +1,82 @@
+#include <stdio.h>
+
 #include "headers/ieee-stuff.h"
 
-char* floatToBinary(float f) {
-    if (f == 0) {
-        return "0 00000000 00000000000000000000000";
+// exceptions: https://docs.oracle.com/cd/E19957-01/806-3568/ncg_handle.html#:~:text=IEEE%20754%20defines%20five%20basic,be%20ignored%20when%20they%20occur
+
+
+struct IEEE754 {
+    unsigned int mantissa: 23;
+    unsigned int exponent: 8;
+    unsigned int sign: 1;
+};
+typedef union {
+    float f;
+    struct IEEE754 ieee;
+} binary;
+
+// taken from https://www.geeksforgeeks.org/program-for-conversion-of-32-bits-single-precision-ieee-754-floating-point-representation/
+void printBinary(int n, int i)
+{
+    int k;
+    for (k = i - 1; k >= 0; k--) {
+ 
+        if ((n >> k) & 1)
+            printf("1");
+        else
+            printf("0");
     }
-    
-    return "not implemented yet";
 }
 
-int isInexact(float resultado) {
+char* floatToBinary(float f) {
+    binary ieee;
+    ieee.f = f;
+    printf("%d ", ieee.ieee.sign);
+    printBinary(ieee.ieee.exponent, 8);
+    printf(" ");
+    printBinary(ieee.ieee.mantissa, 23);
+    printf("\n");
+    return "";
+}
+
+int isInexact(float resultado, int* firstRounded, int* secondRounded) {
+    // if any of the inserted has to suffer rounding
     return 0;
 }
 
-int isDivByZero(float segundoNumero) {
-    return 0;
+void isDivByZero(float primeiroNumero, float segundoNumero, int* divZero) {
+    if (segundoNumero == 0 && primeiroNumero != 0) 
+        *divZero = 1;
 }
 
-int isUnderflow(float resultado) {
-    return 0;
+int infinitySign(float primeiroNumero) {
+    if (primeiroNumero > 0) {
+        return 0; // positive infinity
+    } else {
+        return 1; // negative infinity
+    }
 }
 
-int isOverflow(float resultado) {
-    return 0;
+void isUnderflow(float result, float a, float b, int* underflow) {
+    float underflowLimit = 1.17549435e-38;
+    if (result < underflowLimit || a < underflowLimit || b < underflowLimit) 
+        *underflow = 1;
+}
+
+void isOverflow(float result, float a, float b, int* overflow) {
+    float overflowLimit = 3.40282347e+38;
+    if (result > overflowLimit || a > overflowLimit || b > overflowLimit) 
+        *overflow = 1;
 }
 
 int isInvalidOperation(float resultado) {
     return 0;
+}
+
+void rounder(float f, int* firstRounded, int* secondRounded) {
+    // choose a method? 
+    // return the float rounded or change the binary
+    // if this is used, the inexact operation is flagged
+    printf("not implemented yet");
+
 }
